4dfbd46956bc5dd893ae1ec62743b86a23515276
Kasipeti <113339465+Kasipeti@users.noreply.github.com>
2023-09-18 15:44:34 +0200

    Add files via upload

---
+++ DocuStat/DocuStat/Model.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuStat
+{
+    public class Model
+    {
+        private string _filePath;
+        public string FileContent { get; private set; }
+        public Dictionary<string, int> DistinctWordCount { get; private set; }
+        //Konstruktor
+        public Model(string filePath) 
+        {
+            _filePath = filePath;
+            FileContent = string.Empty;
+            DistinctWordCount = new Dictionary<string, int>();
+        }
+        public void Load() 
+        {
+           FileContent= File.ReadAllText(_filePath);
+            ComputDistincWords();
+
+        }
+        private void ComputDistincWords()
+        {
+            DistinctWordCount.Clear();
+            string[] words= FileContent.Split().Where(s=> s.Length>0).ToArray();
+            for (int i= 0; i < words.Length; i++) 
+            {
+                words[i] = string.Concat(words[i]
+                    .SkipWhile(c=> !char.IsLetter(c)).
+                    Reverse()
+                    .SkipWhile(c => !char.IsLetter(c)).
+                    Reverse()
+                    );
+                if (string.IsNullOrEmpty( words[i]))
+                {
+                    continue;
+                }
+                words[i] = words[i].ToLower();
+                if (DistinctWordCount.ContainsKey(words[i]))
+                {
+                    ++DistinctWordCount[words[i]];
+                }
+                else
+                {
+                    DistinctWordCount.Add(words[i], 1);
+                }
+            }
+
+        }
+    }
+}
---
+++ DocuStat/DocuStat/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace DocuStat
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            string path;
+            do 
+            {
+                Console.WriteLine( "Please enter the file path: " );
+                path = Console.ReadLine() ?? ""; // ha nullérték az in akkor üres string
+
+            }while(System.IO.Path.GetExtension(path) !=".txt" || !System.IO.File.Exists(path) );
+            //System.IO.Path.GetExtension(path) visszadtja az adott fájl kiterjesztését
+            //System.IO.File.Exists(path) megnézi h létezik e az adott fájl a gépen.
+            Model stat=new Model(path);
+            try
+            {
+                stat.Load();
+            }
+            catch (System.IO.IOException ex)
+            {
+                Console.WriteLine("The file cannot be opened");
+                Console.WriteLine(ex.Message);
+                return -1;
+
+            }
+           int minOccurreence =1; int minLenght = 5;
+            int maxLenght = 12;
+            var pairs = stat.DistinctWordCount.
+                Where(p => p.Value >= minOccurreence).
+                Where(p => p.Key.Length >= minLenght).
+                Where(p => p.Key.Length <= maxLenght);
+            foreach (var pair in pairs) 
+            { Console.WriteLine( $"{pair.Key}:{pair.Value}"   );
+                 
+            }
+
+            return 0;
+            
+        }
+    }
+}
